58d04e6d76255964b1b90ee530880b9e03101b75
NineSunDevelopment <nico.saenger@gmail.com>
2015-06-15 19:57:18 +0200

    v1 Shield

---
+++ Assets/Scripts/Technical/EnergyShield.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyShield : MonoBehaviour {	
+
+	private float effectTime;
+	private Renderer ren;
+	private Color normalColor;
+	public Color hitColor;
+	
+	void Awake(){
+		ren = GetComponent<Renderer>();
+		normalColor = ren.material.GetColor("_Color2");
+	}
+	
+	void Update(){
+		effectTime -= Time.deltaTime;
+		
+		ren.material.SetColor( "_Color2", Color.Lerp (normalColor, hitColor, effectTime) );
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		print ("MOTHA FUCKIN HIT!" );
+		effectTime = 1;
+		ren.material.SetColor( "_Color2", hitColor );
+	}
+}
